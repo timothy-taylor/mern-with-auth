44b95ea8aa8120220184a8a1fdd20cc77d4aeb62
Tim <timothy.george.taylor@gmail.com>
2022-08-18 09:57:23 -0700

    FIX: client Auth header in userAPI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,25 @@
 import { SignUp } from "./components/SignUp";
 import { LogIn } from "./components/LogIn";
 import { useLocalStorage } from "./hooks/useLocalStorage";
+import { axiosClient } from "./api/axiosClient.js";
+import { userAPI } from "./api/user.js";
 
 export function App() {
   const { localData } = useLocalStorage();
 
+  const handleSecret = async () => {
+    const request = userAPI.index(localData);
+    try {
+    const response = await axiosClient.request(request);
+    console.log(response);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-slate-700 text-2xl text-white">
+      <button className="p-4 border" onClick={handleSecret}>Secret button</button>
       <LogIn />
       {localData ? null : (
         <>
--- client/src/api/user.js
+++ client/src/api/user.js
@@ -1,3 +1,10 @@
 export const userAPI = {
+  index: (token) => ({
+    method: "GET",
+    url: "/v1/user/",
+    headers: {
+      'Authorization': `Bearer ${token}`,
+    },
+  }),
   new: (credentials) => ({ method: "POST", url: "/v1/user/", data: credentials }),
 }
